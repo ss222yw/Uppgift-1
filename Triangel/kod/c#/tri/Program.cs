67858ad743518318568c0961e1c8ee7149ceafbb
ss222yw <ss222yw@student.lnu.se>
2013-11-20 14:47:46 +0100

    Lagt upp Black och white box testing!

---
+++ Triangel/kod/c#/tri/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tri
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+                 double[] input = new double[3]; 
+            for(int i=0;i<3;i++) 
+          input[i]=double.Parse(args[i]); 
+    
+         Triangle t = new Triangle(input); 
+
+         if(t.isScalene()) Console.WriteLine("Triangeln har inga lika sidor"); 
+          if(t.isEquilateral()) Console.WriteLine("Triangeln är liksidig"); 
+          if(t.isIsosceles()) Console.WriteLine("Triangeln är likbent"); 
+        }
+    }
+}
